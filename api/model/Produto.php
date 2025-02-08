7c3d096204cd26b0438c91d8c196c85ab210bed5
Leonardo Fernandes Ramos <140019222+FernandesLR@users.noreply.github.com>
2025-02-08 19:35:09 -0300

    Add: pasta de backend

---
+++ api/model/Produto.php
@@ -0,0 +1,57 @@
+class Produto {
+    private $id;
+    private $produto;
+    private $preco;
+    private $img;
+    private $title;
+
+    // Getter e Setter para o ID
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    // Getter e Setter para o Produto
+    public function getProduto() {
+        return $this->produto;
+    }
+
+    public function setProduto($produto) {
+        $this->produto = $produto;
+    }
+
+    // Getter e Setter para o Preço
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
+
+    // Getter e Setter para a Imagem
+    public function getImg() {
+        return $this->img;
+    }
+
+    public function setImg($img) {
+        $this->img = $img;
+    }
+
+    // Getter e Setter para o Título
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+}
+
+$p = new Produto();
+
+
+$p->getTitle();
