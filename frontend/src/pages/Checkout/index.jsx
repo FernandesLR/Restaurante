8f82eb95de6bdbafce91587e7514c1858e587858
FernandesLr <leo1ramosf@gmail.com>
2025-08-26 16:49:13 -0300

    Add: Página de checkout

---
+++ frontend/src/pages/Checkout/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Checkout() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-yellow-100">
+            <h1 className="text-4xl font-bold mb-8">Checkout</h1>
+            <p className="text-lg mb-4">Obrigado por comprar conosco!</p>
+            <p className="text-lg">Seu pedido está sendo processado.</p>
+        </div>
+    );
+}
