c8a97b0552c14e76ed8a086ac91c08dbe334a8fa
FernandesLr <leo1ramosf@gmail.com>
2025-09-10 20:12:24 -0300

    Feat: adiciona metodo responsavel por pegar dados dos produtos do banco de dados

--- api/src/repository/ProdutoRepository.php
+++ api/src/repository/ProdutoRepository.php
@@ -1,4 +1,35 @@
 <?php
 
+namespace App\repository;
 
-?>
+use App\model\Produto;
+use PDO;
+
+class ProdutoRepository {
+    private PDO $conexao;
+
+    public function __construct(PDO $conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function getProdutos(): array {
+        $listaPd = [];
+
+        $stmt = $this->conexao->query("SELECT * FROM produtos");
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($rows as $row) {
+            $produto = new Produto();
+            $produto->setId($row['id']);
+            $produto->setDescricao($row['descricao']);
+            $produto->setTitle($row['title']);
+            $produto->setImg($row['img']);
+            $produto->setPreco($row['preco']);
+
+            $listaPd[] = $produto;
+        }
+
+        return $listaPd;
+    }
+}
