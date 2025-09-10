ef5ea916ae1a1f79c6da1e61156f6edb6b7f15a2
FernandesLr <leo1ramosf@gmail.com>
2025-09-10 19:53:01 -0300

    Feat: adiciona classe responsavel por criar a conexao com o banco de dados

---
+++ api/src/config/Conexao.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\config;
+
+use PDO;
+use PDOException;
+
+
+class Conexao{
+
+    public function conectar(){
+        $user = "root";
+        $pass = "root";
+        $dbName = "loja";
+        $local = "localhost";
+        
+        try{
+            return new PDO("mysql:host=$local;dbname=$dbName", $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Se der erro lança um erro ao invés de um warning
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Retorna arrays associativos ao invés de duplicar indices
+                PDO::ATTR_EMULATE_PREPARES => false // Usa prepared statements reais que protegem contra injeções sql
+            ]);
+        }catch(PDOException $e){
+            echo "Erro ao conectar: " + $e;
+        }
+
+    }
+}
+
+?>
