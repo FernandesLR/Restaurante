1d8ec20c11a4e9cf7a8dc00bbaff5d5d6a676dc3
FernandesLr <leo1ramosf@gmail.com>
2025-09-10 20:28:28 -0300

    Feat: adiciona metodo no produtoservice que pega os dados de produtos da sua classe repository

--- api/src/service/ProdutoService.php
+++ api/src/service/ProdutoService.php
@@ -1,8 +1,22 @@
 <?php
 
-namespace App\ProdutoService;
+namespace App\service;
+
+use App\config\Conexao;
+use App\repository\ProdutoRepository;
+
 
 class ProdutoService{
+    protected ProdutoRepository $produtoRepo;
+
+    public function __construct()
+    {
+        $cnx = (new Conexao())->conectar();
+        $this->produtoRepo = new ProdutoRepository($cnx);
+    }
+    public function getProdutos(){
+        return $this->produtoRepo->getProdutos();
+    }
     
 }
 
