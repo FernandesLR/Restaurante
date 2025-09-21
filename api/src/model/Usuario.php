44ecbbc5e670b5525d18af4f476d8aa28fd9505c
FernandesLr <leo1ramosf@gmail.com>
2025-09-21 17:27:31 -0300

    Feat: adiciona o atributo id junto com os seus setters e getters, adiciona também o metodo  jsonserialize

--- api/src/model/Usuario.php
+++ api/src/model/Usuario.php
@@ -1,22 +1,43 @@
 <?php
 
-class Usuario {
+namespace App\model;
+use JsonSerializable;
 
+class Usuario implements \JsonSerializable{
+
+    private $id;
     private $cpf;
     private $nome;
     protected $email;
     protected $senha;
 
     // Construtor
-    public function __construct($cpf, $nome, $email, $senha) {
+    public function __construct($cpf = null, $nome = null, $email = null, $senha = null) {
         $this->cpf = $cpf;
         $this->nome = $nome;
         $this->email = $email;
         $this->senha = $senha;
     }
 
+
+    public function jsonSerialize(): array {
+        return [
+            'id' => $this->getId(),
+            'nome' => $this->getNome(),
+            'email' => $this->getEmail()
+        ];
+    }
+
     // Getters e Setters
-    public function getCpf() {
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId($i){
+        $this->id = $i;
+
+    }
+
+    public function getCpf(): string{
         return $this->cpf;
     }
 
@@ -24,7 +45,7 @@ class Usuario {
         $this->cpf = $cpf;
     }
 
-    public function getNome() {
+    public function getNome(): string {
         return $this->nome;
     }
 
@@ -32,7 +53,7 @@ class Usuario {
         $this->nome = $nome;
     }
 
-    public function getEmail() {
+    public function getEmail(): string{
         return $this->email;
     }
 
@@ -40,7 +61,7 @@ class Usuario {
         $this->email = $email;
     }
 
-    public function getSenha() {
+    public function getSenha(): string {
         return $this->senha;
     }
 
