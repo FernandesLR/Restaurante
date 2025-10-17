ffe8c3f68199736046d72970c7aff1140c97ba0d
FernandesLr <leo1ramosf@gmail.com>
2025-10-17 11:35:59 -0300

    fix: corrige metodo valida email e valida senha pois nao estava aceitando dados validos

--- api/src/service/UsuarioService.php
+++ api/src/service/UsuarioService.php
@@ -14,35 +14,31 @@ class UsuarioService {
     }
 
     public function login(string $email, string $senha): ?Usuario {
-        // valida formato do email
         if (!$this->verificaEmail($email)) {
             return null;
         }
 
-        // busca usuário no banco
         $usuario = $this->usuRep->buscaEmail($email);
+        if (!$usuario) return null;
 
-        if (!$usuario) {
-            return null;
-        }
-
-        // compara senha digitada com hash do banco
         if (!$this->compararSenha($senha, $usuario->getSenha())) {
             return null;
         }
-        $usuario->setSenha(null);
 
+        $usuario->setSenha(null);
         return $usuario;
     }
 
+    
     private function verificaEmail(string $email): bool {
-        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+        return str_ends_with(strtolower($email), '@gmail.com') ||
+               str_ends_with(strtolower($email), '@hotmail.com');
     }
 
-    // usado só no cadastro
+    
     public function verificaSenhaForte(string $senha): bool {
-        if(!$senha) return false;
-        return preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $senha) === 1;
+        if (strlen($senha) < 8) return false;
+        return true;
     }
 
     private function compararSenha(string $senhaDigitada, string $hashDoBanco): bool {
@@ -51,32 +47,21 @@ class UsuarioService {
 
     public function cadastrar(string $email, string $senha, string $nome, string $cpf): ?bool {
         
-        // valida email
-        if (!$this->verificaEmail($email)) {
+        
+        if (!$this->verificaEmail($email) || !$this->verificaSenhaForte($senha)) {
             return null;
         }
 
-        // valida senha forte
-        if (!$this->verificaSenhaForte($senha)) {
-            return null;
-        }
-
-        // criptografa senha
         $hash = password_hash($senha, PASSWORD_BCRYPT);
 
-        // cria objeto Usuario
         $usuario = new Usuario();
         $usuario->setEmail($email);
         $usuario->setNome($nome);
         $usuario->setCpf($cpf);
         $usuario->setSenha($hash);
 
-        // persiste via repository
         $salvo = $this->usuRep->salvar($usuario);
-
         return $salvo;
     }
-
 }
-
 ?>
